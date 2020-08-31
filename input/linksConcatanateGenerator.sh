8f514df4ce059c83b96d816fbc7a7efe6477e47a
adi <adithyan.ilangovan@bitmovin.com>
2020-08-31 12:55:20 +0200

    Modify the concatanate generator to automate stuff.

--- input/linksConcatanateGenerator.sh
+++ input/linksConcatanateGenerator.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
 
-for i in {0..346}
-  do
-    y=$(printf "%05d" $i)
-    echo "file '/home/adithyan_ilangovan_bitmovin_com/EN-8066/output/output/NARUTO/segments/waifu2x_caffe_2x/video_${y}_scale=2_CRF=17_libx264_default_waifu2x_caffe.mkv'" >> /home/adithyanilangovan/Repos/video2x/input/linkNarutoConcatanate_waifu2x_caffe_2x.txt
+for filter in waifu2x_caffe
+do
+  echo ${filter}
+    for scale_factor in 2
+    do
+        for segment_number in {0..346}
+          do
+            formatted_segment_number=$(printf "%05d" $segment_number)
+            output_file_directory="/home/adithyan_ilangovan_bitmovin_com/EN-8066/output/output/NARUTO/segments/${filter}_${scale_factor}x"
+            output_file_name="video_${formatted_segment_number}_scale=${scale_factor}_CRF=17_libx264_default_${filter}.mkv"
+
+            sudo mkdir -p ${output_file_directory}
+
+            directory_to_store="/home/adithyanilangovan/Repos/video${scale_factor}x/input/"
+            concatante_file_to_store="concatante_${filter}_${scale_factor}.txt"
+
+            echo "file '${output_file_directory}/${output_file_name}'" >> ${directory_to_store}${concatante_file_to_store}
+
+        done
+    done
 done
